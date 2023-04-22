6d74a2cefe81a3eb29762b82efb4779bc607bd3c
Sara <sara.jesus@unoesc.edu.br>
2023-04-22 09:11:52 -0300

    ex2

---
+++ aula3/Ex2.js
@@ -0,0 +1,5 @@
+
+const nome = prompt("Qual é o seu nome?");
+const idade = parseInt(prompt("Qual é a sua idade?"));
+const diasVividos = idade * 365;
+alert(`${nome}, você já viveu ${diasVividos} dias!`);
